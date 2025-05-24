4e205b26ee496da3ad3202f9296811d594b58821
mohsin munir <mohsinmunir150@gmail.com>
2025-05-24 11:45:15 +0500

    Caching dynamic and static data rendring

--- codewithmosh-myfirstapp/app/users/page.tsx
+++ codewithmosh-myfirstapp/app/users/page.tsx
@@ -1,9 +1,27 @@
+// app/user/page.tsx
 import React from 'react'
 
-const UserPage = () => {
-  return (
-    <div>Hello New User</div>
-  )
+interface User {
+  id: number;
+  title: string;
+  url: string;
 }
 
-export default UserPage
+const UserPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=10')
+const users: User[] = await res.json();
+
+  return (
+    <div>
+      <h1>Users</h1>
+      {users.map(user => (
+        <div key={user.id}>
+          <h2>{user.title}</h2>
+          <img src={user.url} alt={user.title} width={200} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UserPage;
